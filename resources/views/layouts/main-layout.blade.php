f5de715504cd540a86713df6abce4353f03270e8
Jesús Alfredo Reyes Vargas <jesus.reyes@protonmail.ch>
2016-11-27 21:06:59 -0400

    Voy creando parte de las views, luego empiezo con los controladores.

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+@include('navbar')
+
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name="author" content='Jonas Gonzalez, Jesús Reyes, Edwin Vargas, Yosswan Vecino'>
+        <meta name="description" content="Proyecto para Bases de Datos I. 2016-1. UNET">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Proyecto DB 20161</title>
+    </head>
+    <body>
+        @yield('navbar')
+
+        @yield('content')
+    </body>
+</html>
