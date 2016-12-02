aecf0fda772887de151586c73bc4f94769b3ec4d
AlfredoJonas <jonas.gonzalez@unet.edu.ve>
2016-12-02 19:04:22 -0400

    Se borro todo el repo para volver a iniciar el proyecto bien

--- resources/views/layouts/error.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="col-md-12">
-    <div class="panel panel-primary">
-        <div class="panel-heading">Sorry my friend!</div>
-        <div class="panel-body">
-            Ha habido un error :-(
-        </div>
-    </div>
-</div>
