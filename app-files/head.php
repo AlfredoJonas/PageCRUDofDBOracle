c256e6917c809425b6459e6f195254e2bee49936
Edwin Vargas <onix.edwin18@gmail.com>
2016-12-06 14:23:35 -0430

    Header creado

--- app-files/head.php
+++ app-files/head.php
@@ -1,4 +1,9 @@
-<meta charset="UTF-8" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
 <title>Odontóloga DientesDeSable</title>
 
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
