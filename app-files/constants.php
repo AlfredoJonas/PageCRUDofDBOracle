d807a67672eb18e75aeb43db3f901608e74280b3
Edwin Vargas <onix.edwin18@gmail.com>
2016-12-07 17:13:39 -0400

    Agregado archivo de constantes para las credenciales, rutas, consultas y demas

---
+++ app-files/constants.php
@@ -0,0 +1,36 @@
+<?php
+
+  //Credenciales
+  define('DB', '//localhost/XE');
+  define('$DB_USER', 'PROYECTODB20161');
+  define('$DB_PASS', '123');
+  define('$DB_CHAR', 'AL32UTF8');
+
+  //Rutas
+  define("RUTA_EMPLEADOS", "empleados");
+  define("RUTA_PACIENTES", "pacientes");
+  define("RUTA_INVENTARIO", "inventario");
+  define("RUTA_HORARIOS", "horarios");
+
+  //Indicadores
+  define("LISTA_EMPLEADOS", "solicitudListaEmpleados");
+  define("LISTA_ESPECIALIZACIONES", "solicitudListaEspecializaciones");
+  define("LISTA_PACIENTES", "solicitudListaPacientes");
+  define("LISTA_INVENTARIO", "solicitudListaInventario");
+  define("HORARIO_DOCTOR_FECHA", "solicitudHorarioDoctorRangoDeFechas");
+  define("HORARIO_DOCTOR_DIA", "solicitudHorarioDoctorDia");
+
+  //SQL
+  define("LISTA_EMPLEADOS_SQL",
+   " aqui va la super consulta "
+  , true);
+  define("LISTA_ESPECIALIZACIONES_SQL",
+   " aqui va la super consulta "
+  , true);
+  define("LISTA_PACIENTES_SQL",
+   " aqui va la super consulta "
+  , true);
+  define("LISTA_INVENTARIO_SQL",
+   " aqui va la super consulta "
+  , true);
+ ?>
