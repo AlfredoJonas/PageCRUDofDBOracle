71994eef86b23ce8a4290571d72924f27cd92faa
Edwin Vargas <onix.edwin18@gmail.com>
2016-12-07 16:05:57 -0400

    Super modificacion al querys-php

--- app-files/querys.php
+++ app-files/querys.php
@@ -1,5 +1,6 @@
 <?php
   function preprocesar_cosultas($tipo_query, $from_page, $data_adicional){
+
     $response = array();
     $consulta = '';
     $ejecutar_consulta = 0;
@@ -14,55 +15,65 @@
       }
     }
 
-    if(strcmp($tipo_query, 'solicitudListaEspecializaciones') == 0 && strcmp($from_page, 'empleados') == 0){
-      $consulta = $GLOBALS["solicitudListaEspecializaciones"];
-      $ejecutar_consulta = 1;
-    }
-    else{
-      if(strcmp($tipo_query, 'solicitudListaEmpleados') == 0 && strcmp($from_page, 'empleados') == 0){
-        $consulta = $GLOBALS["solicitudListaEmpleados"];
-        $ejecutar_consulta = 1;
-      }
-      else{
-        if(strcmp($tipo_query, 'solicitudListaPacientes') == 0 && strcmp($from_page, 'pacientes') == 0){
-          $consulta = $GLOBALS["solicitudListaPacientes"];
-          $ejecutar_consulta = 1;
+
+    switch ($from_page) {
+      case 'empleados':
+        switch ($tipo_query) {
+          case 'solicitudListaEmpleados':
+            $consulta = $GLOBALS["solicitudListaEmpleados"];
+            $ejecutar_consulta = 1;
+          break;
+          case 'solicitudListaEspecializaciones':
+            $consulta = $GLOBALS["solicitudListaEmpleados"];
+            $ejecutar_consulta = 1;
+          break;
         }
-        else{
-          if(strcmp($tipo_query, 'solicitudListaInventario') == 0 && strcmp($from_page, 'inventario') == 0){
+      break;
+
+      case 'pacientes':
+        switch ($tipo_query) {
+          case 'solicitudListaPacientes':
+            $consulta = $GLOBALS["solicitudListaPacientes"];
+            $ejecutar_consulta = 1;
+          break;
+        }
+      break;
+
+      case 'inventario':
+        switch ($tipo_query) {
+          case 'solicitudListaInventario':
             $consulta = $GLOBALS["solicitudListaInventario"];
             $ejecutar_consulta = 1;
-          }
-          else{
-            if(strcmp($tipo_query, 'solicitudHorarioDoctorDia') == 0 && strcmp($from_page, 'horarios') == 0){
-              if($data_es_arreglo != 0){
-                if(isset($data["fecha"])){
-                  $consulta = "SELECT TO_CHAR(nombre_variable,'hh24:mi')
-                               FROM nombre_tabla
-                               WHERE TO_CHAR(FECHA,'DD/MM/YYYY') = ".$data["fecha"]."
-                               ORDER BY FECHA;";
-                  $ejecutar_consulta = 1;
-                }
-              }
-            }
-            else{
-              if(strcmp($tipo_query, 'solicitudHorarioDoctorRangoDeFechas') == 0 && strcmp($from_page, 'horarios') == 0){
-                if($data_es_arreglo != 0)
-                  if(isset($data["fechaInicio"]) && isset($data["fechaFin"])){
-                    $consulta = "SELECT TO_CHAR(nombre_variable,'hh24:mi')
-                                 FROM nombre_tabla
-                                 WHERE FECHA BETWEEN TO_DATE(".$data["fechaInicio"].") AND TO_DATE(".$data["fechaFin"].")
-                                 ORDER BY FECHA;";
-                    $ejecutar_consulta = 1;
-                  }
-              }
-            }
-
-          }
+          break;
         }
-      }
-    }
+      break;
 
+      case 'horarios':
+        switch ($tipo_query) {
+          case 'solicitudHorarioDoctorRangoDeFechas':
+            if($data_es_arreglo != 0)
+              if(isset($data["fechaInicio"]) && isset($data["fechaFin"])) {
+                $consulta = "SELECT TO_CHAR(nombre_variable,'hh24:mi')
+                             FROM nombre_tabla
+                             WHERE FECHA BETWEEN TO_DATE(".$data["fechaInicio"].") AND TO_DATE(".$data["fechaFin"].")
+                             ORDER BY FECHA;";
+                $ejecutar_consulta = 1;
+              }
+          break;
+
+          case 'solicitudHorarioDoctorDia':
+            if($data_es_arreglo != 0)
+              if(isset($data["fecha"])){
+                $consulta = "SELECT TO_CHAR(nombre_variable,'hh24:mi')
+                             FROM nombre_tabla
+                             WHERE TO_CHAR(FECHA,'DD/MM/YYYY') = ".$data["fecha"]."
+                             ORDER BY FECHA;";
+                $ejecutar_consulta = 1;
+              }
+          break;
+        }
+      break;
+    }
 
     if($ejecutar_consulta != 0){
       $response = exec_query($consulta);
