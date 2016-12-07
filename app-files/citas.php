ae39198e70c68a6f0d57006cbc949a8a6bbc4532
Jesús Alfredo Reyes Vargas <jesus.reyes@unet.edu.ve>
2016-12-07 14:25:00 -0400

    Añado citas.php

---
+++ app-files/citas.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include('head.php'); ?>
+	</head>
+	<body>
+		<?php include('header.php'); ?>
+			<div class="container-fluid">
+				
+			</div>
+		<?php include('footer.php'); ?>
+	</body>
+</html>
