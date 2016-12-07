abff5dc2e027888d83b67782d983d31af6e310e0
Jesús Alfredo Reyes Vargas <jesus.reyes@unet.edu.ve>
2016-12-07 16:48:11 -0400

    Subo los cambios en las citas antes de irme para la casa.

--- app-files/citas.php
+++ app-files/citas.php
@@ -9,11 +9,105 @@
         <div class="row">
 					<div class="col-sm-6">
 						<div class="panel panel-primary">
-						  <div class="panel-heading">Registro del empleado</div>
+						  <div class="panel-heading">Administracion de citas</div>
 						  <div class="panel-body">
-							<div id="form-messages" class="alert .alert-dismissible fade in hidden">
-								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-							</div>
+								<div id="form-messages" class="alert .alert-dismissible fade in hidden">
+									<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+								</div>
+
+
+									<form class="formCitas" action="ajax-handler.php" role="form">
+										<div class="form-group row">
+											<label for="identificadorInput"class="col-sm-4 col-form-label">Identificador</label>
+											<small id="select-esp-help" class="form-text text-muted col-sm-8">Éste es el identificador de la cita.</small>
+											<div class="col-sm-9">
+												<input type="text" id="identificadorInput" name="identificadorInput" class="form-control">
+											</div>
+											<div class="col-sm-3">
+												<input type="button" id="buscarIdentificadorInput" name="buscarIdentificadorInput" class="form-control" value="Buscar cita">
+											</div>
+										</div>
+
+										<div class="form-group row">
+											<label for="pacienteSeleccion"class="col-sm-4 col-form-label">Pacientes</label>
+											<small id="select-esp-help" class="form-text text-muted col-sm-8">Seleccione un paciente para la cita (debe estar registrado).</small>
+											<div class="col-sm-12">
+												<select id="pacienteSeleccion" name="pacienteSeleccion" class="form-control">
+													<option value="Leonardo da Vinci">Leonardo da Vinci</option>
+													<option value="Albert Einstein">Albert Einstein</option>
+												</select>
+											</div>
+										</div>
+
+										<div class="form-group row">
+											<label for="doctorSeleccion"class="col-sm-4 col-form-label">Doctores</label>
+											<small id="select-esp-help" class="form-text text-muted col-sm-8">Seleccione un doctor para la cita (debe estar registrado).</small>
+											<div class="col-sm-12">
+												<select id="doctorSeleccion" name="doctorSeleccion" class="form-control">
+													<option value="Isaac Newton">Isaac Newton</option>
+													<option value="Marie Curie">Marie Curie</option>
+												</select>
+											</div>
+										</div>
+
+										<div class="form-group row">
+											<label for="fechaInput"class="col-sm-4 col-form-label">Fecha y hora de la cita</label>
+												<div class="col-sm-4">
+													<input type="date" id="fechaInput" name="fechaInput" class="form-control">
+												</div>
+												<div class="col-sm-4">
+													<select id="horaInput" name="horaInput" class="form-control">
+														<option value="8:00">8:00</option>
+														<option value="8:30">8:30</option>
+														<option value="9:00">9:00</option>
+														<option value="9:30">9:30</option>
+														<option value="10:00">10:00</option>
+														<option value="10:30">10:30</option>
+														<option value="11:00">11:00</option>
+														<option value="11:30">11:30</option>
+														<option value="12:00">12:00</option>
+														<option value="12:30">12:30</option>
+														<option value="13:00">13:00</option>
+														<option value="13:30">13:30</option>
+														<option value="14:00">14:00</option>
+														<option value="14:30">14:30</option>
+														<option value="15:00">15:00</option>
+														<option value="15:30">15:30</option>
+														<option value="16:00">16:00</option>
+														<option value="16:30">16:30</option>
+														<option value="17:00">17:00</option>
+														<option value="17:30">17:30</option>
+														<option value="18:00">18:00</option>
+														<option value="18:30">18:30</option>
+													</select>
+												</div>
+										</div>
+
+										<div class="form-group row">
+											<label for="odontogramaInput"class="col-sm-4 col-form-label">Odontograma</label>
+											<small id="select-esp-help" class="form-text text-muted col-sm-8">Inserte el URL de la imagen.</small>
+											<div class="col-sm-12">
+												<input type="text" id="odontogramaInput" name="odontogramaInput" class="form-control">
+											</div>
+										</div>
+
+										<div class="form-group row">
+											<label for="presupuestoInput"class="col-sm-4 col-form-label">Presupuesto</label>
+											<small id="select-esp-help" class="form-text text-muted col-sm-8">En $</small>
+											<div class="col-sm-12">
+												<input type="text" id="presupuestoInput" name="presupuestoInput" class="form-control">
+											</div>
+										</div>
+
+										<div class="form-group">
+											<input type="submit" class="form-control"name="guardar" value="Agregar">
+										</div>
+
+										<div class="form-group">
+											<input type="submit" class="form-control"name="update" value="Actualizar datos">
+										</div>
+
+									</form>
               </div>
             </div>
           </div>
