19d498481fb062cacdff50395c306bc4d1d3b8d8
Edwin Vargas <onix.edwin18@gmail.com>
2016-12-07 22:06:51 -0400

    Me faltaba agregar el archivo a la cabecera

--- app-files/head.php
+++ app-files/head.php
@@ -16,6 +16,6 @@
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-	<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="js/ajax-submits.js"></script>
 	<script type="text/javascript" src="js/ajax-requests.js"></script>
 '; ?>
