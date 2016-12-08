e2c40c755a2ce15f4ccbf8c2d945b624c67d6a24
Edwin Vargas <onix.edwin18@gmail.com>
2016-12-07 22:51:10 -0400

    Agregada la pagina superdinamica para las consultas heavy

---
+++ app-files/informacion.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include('head.php'); ?>
+	</head>
+	<body>
+		<?php include('header.php'); ?>
+      
+		<?php include('footer.php'); ?>
+	</body>
+</html>
