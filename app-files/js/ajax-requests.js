1151102425e9797ee01064562705cb5be66d48f8
Jesús Alfredo Reyes Vargas <jesus.reyes@protonmail.ch>
2016-12-07 23:11:41 -0400

    Agrego los cambios que permitena ctualizar el formulario de las citas cuando se ingresa un identificador de la cita apra ubicarlo. Edwin cuidado con esto, no sé que haya cambiado yo aquí que ud necesite

---
+++ app-files/js/ajax-requests.js
@@ -0,0 +1,160 @@
+$(document).ready(function() {
+    requestPacientes();
+    requestDoctores();
+	  requestEspecializaciones();
+});
+
+function parse_data(data, tipo) {
+  if (tipo === 'multiple_select') {
+    //Esto es porque yo tengo en mi BD una tabla con una columna usuario
+    //pero ustedes me entienden
+    return new Option(data.USUARIO.toLowerCase(), data.USUARIO);
+  }
+  else{
+    if (tipo === 'single_select'){
+    	return new Option(data.USUARIO.toLowerCase(), data.USUARIO);
+    }
+  }
+}
+
+function requestEspecializaciones() {
+  var ruta = $('.especializaciones').data("ruta");
+  var consulta = $('.especializaciones').data("consulta");
+  var formMessages = $('#form-messages');
+
+  $.ajax({
+	  type: 'POST',
+	  url: 'ajax-handler.php',
+	  data: {ruta: ruta, consulta: consulta}
+	})
+
+  .done(function(response) {
+    $('.especializaciones').addClass('filled');
+
+    for(var key in response) {
+      document.querySelector('.especializaciones').options.add(parse_data(response[key], 'multiple_select'));
+    }
+	})
+
+  .fail(function(data) {
+    formMessages.removeClass('hidden');
+    formMessages.addClass('alert-danger');
+
+    if (data.responseText !== '')
+      formMessages.text(data.responseText);
+    else
+      formMessages.text('Oops! An error occured.');
+	});
+}
+
+function requestPacientes(){
+	var ruta = $('.pacienteSeleccion').data("ruta");
+	var consulta = $('.pacienteSeleccion').data("consulta");
+  var formMessages = $('#form-messages');
+
+  $.ajax({
+    type: 'POST',
+    url: 'ajax-handler.php',
+    data: {ruta: ruta, consulta: consulta}
+  })
+
+  .done(function(response) {
+      $('.pacienteSeleccion').addClass('filled');
+
+      for(var key in response) {
+        document.querySelector('.pacienteSeleccion').options.add(parse_data(response[key], 'single_select'));
+      }
+  })
+
+  .fail(function(data) {
+    formMessages.removeClass('hidden');
+    formMessages.addClass('alert-danger');
+
+    if (data.responseText !== '')
+      formMessages.text(data.responseText);
+    else
+      formMessages.text('Oops! An error occured.');
+  });
+}
+
+function requestDoctores(){
+	var ruta = $('.doctorSeleccion').data("ruta");
+	var consulta = $('.doctorSeleccion').data("consulta");
+  var formMessages = $('#form-messages');
+
+  $.ajax({
+    type: 'POST',
+    url: 'ajax-handler.php',
+    data: {ruta: ruta, consulta: consulta}
+  })
+  .done(function(response){
+    $('.doctorSeleccion').addClass('filled');
+
+    for(var key in response) {
+      document.querySelector('.doctorSeleccion').options.add(parse_data(response[key], 'single_select'));
+    }
+  })
+  .fail(function(data) {
+    formMessages.removeClass('hidden');
+    formMessages.addClass('alert-danger');
+
+    if (data.responseText !== '')
+      formMessages.text(data.responseText);
+    else
+      formMessages.text('Oops! An error occured.');
+  });
+}
+
+function requestInformacionCita(){
+  var id_cita = parseInt($(".identificadorInput").text());
+  var ruta = $('.doctorSeleccion').data("ruta");
+	var consulta = $('.doctorSeleccion').data("consulta");
+  var formMessages = $('#form-messages');
+
+  $.ajax({
+    type: 'POST',
+    url: 'ajax-handler.php',
+    data: {ruta: ruta, consulta: consulta}
+  })
+  .done(function(response){
+      if(response.paciente){
+        $('.doctorSeleccion').remove();
+
+        $('.doctorSeleccionDiv').append('<input type="text" id="doctorInput" name="doctorInput" class="form-control doctorInput" readonly text="'+ response.doctor +'">');
+      }
+
+      if(response.doctor){
+        $('.pacienteSeleccion').remove();
+
+        $('.pacienteSeleccionDiv').append('<input type="text" id="pacienteInput" name="pacienteInput" class="form-control pacienteInput" readonly text="'+ response.paciente +'">');
+      }
+
+      if(response.fecha){
+        $('.fechaInput').remove();
+        $('.horaInput').remove();
+
+        $('.fechaHoraDiv').append('<input type="text" id="fechaHoraInput" name="fechaHoraInput" class="form-control fechaHoraInput" readonly text="'+ response.fecha +'">');
+      }
+
+      if(response.odontograma){
+        $('.odontogramaInput').remove();
+        $('.presupuestoInput').attr("text",response.odontograma + "")
+        $('.odontogramaInput').attr("readonly","readonly");
+      }
+
+      if(response.presupuesto){
+        $('.presupuestoInput').remove();
+        $('.presupuestoInput').attr("text",response.presupuesto + "")
+        $('.presupuestoInput').attr("readonly","readonly");
+      }
+  })
+  .fail(function(data) {
+    formMessages.removeClass('hidden');
+    formMessages.addClass('alert-danger');
+
+    if (data.responseText !== '')
+      formMessages.text(data.responseText);
+    else
+      formMessages.text('Oops! An error occured.');
+  });
+}
