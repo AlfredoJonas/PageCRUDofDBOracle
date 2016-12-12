7544e66ea4a7b1ff512c00e4d2c54a8721c99eac
Jesús Alfredo Reyes Vargas <jesus.reyes@protonmail.ch>
2016-12-12 06:36:44 -0400

    Arreglo metidota de pata

--- app-files/graficas-handler.php
+++ app-files/graficas-handler.php
@@ -48,14 +48,17 @@
 				
 				case 'GANANCIA_POR_MES':
 					if(isset($_POST["mes"]) && isset($_POST["ano"])){
-						$consulta = oci_parse($conn, 'begin :r := myfunc(:mes,:anho); end;');
+						$consulta = oci_parse($conn, 'begin :r := GANANCIA(:mes,:anho); end;');
 						oci_bind_by_name($stid, ':mes', $_POST["mes"]);
 						oci_bind_by_name($stid, ':anho', $_POST["ano"], 40);
 					}
 					break;
 
-				case 3:
-					'GRAFICA_X'
+				case 'GRAFICA_X':
+					if(isset($_POST["ano"])){
+						$consulta = oci_parse($conn, 'begin :r := GRAFICOANIO(:anho); end;');
+						oci_bind_by_name($stid, ':anho', $_POST["ano"], 40);
+					}
 					break;
 
 				case 4:
